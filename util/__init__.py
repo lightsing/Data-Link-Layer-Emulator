eff0fcf0d4fe285d428b99686d8e380f7b220878
lightsing <light.tsing@gmail.com>
2017-11-20 20:33:36 +0800

    add constant import

--- util/__init__.py
+++ util/__init__.py
@@ -1,9 +1,11 @@
 from .ip_mac import get_local_ipv4_address, \
     ip2mac, mac2ip, mac_aton, mac_ntoa
 from .frame import pack_frame, unpack_frame
+from .frame import MTU, PAYLOAD_MTU
 
 
 __all__ = ['get_local_ipv4_address',
            'ip2mac', 'mac2ip',
            'mac_ntoa', 'mac_aton',
-           'pack_frame', 'unpack_frame']
+           'pack_frame', 'unpack_frame',
+           'MTU', 'PAYLOAD_MTU']

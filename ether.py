a4f80fb8a590f8b933612e9f192b9472a80c251a
lightsing <light.tsing@gmail.com>
2017-11-20 20:34:01 +0800

    add Income_Handler class

--- ether.py
+++ ether.py
@@ -1,21 +1,41 @@
 import json
-
+import threading
 from socket import *
 
 from util import *
 
+
+class IncomeHandler(threading.Thread):
+    def __init__(self, link_layer):
+        super(IncomeHandler, self).__init__()
+        self.link_layer = link_layer
+
+    def run(self):
+        while True:
+            frame, address = self.link_layer.sock.recvfrom(MTU)
+            if address[1] != self.link_layer.config['port']:
+                # ignore any non common port
+                continue
+            mac, payload = unpack_frame(frame)
+            if mac not in self.link_layer.mac_table:
+                self.link_layer.mac_table[mac] = address[0]
+            # calling callback
+            self.link_layer.callback(payload)
 
 class LinkLayer:
     """
     LinkLayer class
     """
 
-    def __init__(self, callback):
+    def __init__(self, callback, MAC=None):
         with open('config.json') as reader:
             self.config = json.load(reader)
 
         self.listen = get_local_ipv4_address()
-        self.MAC = ip2mac(self.listen)
+        if MAC is not None:
+            self.MAC = MAC
+        else:
+            self.MAC = ip2mac(self.listen)
         self.mac_table = dict()
         for ip in self.config['devices']:
             self.mac_table[ip2mac(ip)] = ip
